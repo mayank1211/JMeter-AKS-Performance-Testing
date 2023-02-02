2365757d6b5f87aaefc884c2a9977c1c2b0f0b38
Mayank Patel <mayankkumar.patel1@nhs.net>
2023-02-02 13:20:38 +0000

    Initial Performance Testing Platform Commit

---
+++ AzureDevopsProjectSetup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# # Log into Azure DevOps using PAT Token
+# az login
+# az devops configure --defaults organization=https://dev.azure.com/$1
+
+# # Create devops project
+# az devops project create \
+#     --name "Performance_Testing" \
+#     --description "Repository to hold and run JMeter performance tests" \
+#     --source-control "git" \
+#     --visibility "private"
+
+# Push code to main branch
+az repos create --name "Performance_Testing"
+
+git init
+git add .
+git commit -m "Initial Performance Testing Platform Commit"
+git branch -M main
+git remote add origin https://$1@dev.azure.com/$1/Performance_Testing/_git/Performance_Testing
+git push -u origin --all
+git pull
+
+# # Create build pipeline for self-hosted agents
+# az pipelines create --name
+#                     [--branch]
+#                     [--description]
+#                     [--detect {false, true}]
+#                     [--folder-path]
+#                     [--org]
+#                     [--project]
+#                     [--queue-id]
+#                     [--repository]
+#                     [--repository-type {github, tfsgit}]
+#                     [--service-connection]
+#                     [--skip-first-run {false, true}]
+#                     [--yaml-path]
+# Create build pipeline for grafana-influx
+
+# Create build pipeline for test runs
+
+# Create build pipeline for azure pipeline export - to other projects with shared aks connections
