86099422bc525d724a3981f6dbf2f1043f4d7c8e
Mayank Patel <mayankkumar.patel1@nhs.net>
2023-02-02 16:37:21 +0000

    Add automated azure devop resource creation for the platform - minor changes

--- AzureDevopsProjectSetup.sh
+++ AzureDevopsProjectSetup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# # Log into Azure DevOps using PAT Token
-# az login
-# az devops configure --defaults organization=https://dev.azure.com/$1
+# Log into Azure DevOps using PAT Token
+az login
+az devops configure --defaults organization=https://dev.azure.com/$1
 
-# # Create devops project
-# az devops project create \
-#     --name "Performance_Testing" \
-#     --description "Repository to hold and run JMeter performance tests" \
-#     --source-control "git" \
-#     --visibility "private"
+# Create devops project
+az devops project create \
+    --name "Performance_Testing" \
+    --description "Repository to hold and run JMeter performance tests" \
+    --source-control "git" \
+    --visibility "private"
 
 # Push code to main branch
 az repos create --name "Performance_Testing"
@@ -20,22 +20,54 @@ git commit -m "Initial Performance Testing Platform Commit"
 git remote add origin https://$1@dev.azure.com/$1/Performance_Testing/_git/Performance_Testing
 git push -u origin --all
 
-# # Create build pipeline for self-hosted agents
-# az pipelines create --name
-#                     [--branch]
-#                     [--description]
-#                     [--detect {false, true}]
-#                     [--folder-path]
-#                     [--org]
-#                     [--project]
-#                     [--queue-id]
-#                     [--repository]
-#                     [--repository-type {github, tfsgit}]
-#                     [--service-connection]
-#                     [--skip-first-run {false, true}]
-#                     [--yaml-path]
+# Create build pipeline for self-hosted agents
+az pipelines create \
+    --name "Self-hosted-ubuntu-agents-CI" \
+    --branch "main" \
+    --description "Pipeline to deploy self-hosted agents to AKS CLuster for long duration tests" \
+    --detect "false" \
+    --folder-path \
+    --org "https://$1@dev.azure.com/$1" \
+    --project "Performance_Testing" \
+    --repository "Performance_Testing" \
+    --repository-type "tfsgit" \
+    --yml-path "azure-self-hosted-agent.yml"
+
 # Create build pipeline for grafana-influx
+az pipelines create \
+    --name "InfluxDB-Grafana-CI" \
+    --branch "main" \
+    --description "Pipeline to deploy influxDB and grafana for jmeter monitoring to AKS CLuster" \
+    --detect "false" \
+    --folder-path \
+    --org "https://$1@dev.azure.com/$1" \
+    --project "Performance_Testing" \
+    --repository "Performance_Testing" \
+    --repository-type "tfsgit" \
+    --yml-path "grafana-influx-agent.yml"
 
 # Create build pipeline for test runs
+az pipelines create \
+    --name "Performance-Testing-CI" \
+    --branch "main" \
+    --description "Pipeline to run JMeter tests using AKS cluster" \
+    --detect "false" \
+    --folder-path \
+    --org "https://$1@dev.azure.com/$1" \
+    --project "Performance_Testing" \
+    --repository "Performance_Testing" \
+    --repository-type "tfsgit" \
+    --yml-path "performance-testing.yml"
 
 # Create build pipeline for azure pipeline export - to other projects with shared aks connections
+az pipelines create \
+    --name "Export-Performance-Testing-Pipeline-CI" \
+    --branch "main" \
+    --description "Pipeline to export performace testing pipeline to other projects within azure Org with shared variables / AKS Cluster" \
+    --detect "false" \
+    --folder-path \
+    --org "https://$1@dev.azure.com/$1" \
+    --project "Performance_Testing" \
+    --repository "Performance_Testing" \
+    --repository-type "tfsgit" \
+    --yml-path "azure-export-pipeline.yml"
--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # Performance Testing Platform!
-[x] - Allow multiple csv files to run (1 per pod)
-[x] - Allow single csv file to run across pods
-[x] - Allow Only JMX file without CSV to run 
-[ ] - Azure terraform based infrastructure deployment
+
+- [x] Allow multiple csv files to run (1 per pod)
+- [x] Allow single csv file to run across pods
+- [x] Allow Only JMX file without CSV to run
+- [x] Azure terraform based infrastructure deployment (CLI locally)
+- [ ] Add following variables to pipelines to shell script (EXPORT, RUN TEST, SELF-HOSTED, GRAFANA)
+- - [ ] Kubernetes Service Connection
+- - [ ] Azure PAT Token
+- - [ ] Container Registry User & Password
+
 # Installation
+
 ## Setting Up Pipeline
+
 ## Adding Self-Hosted Azure Agent
+
 ## Adding Kubernetes Service Connection
+
 # Additional guidance in setting JMeter properties
+
 [JMeter Official Documentation](https://jmeter.apache.org/usermanual/functions.html#__P)  
 [VSkills JMeter Properties and Variables](https://www.vskills.in/certification/tutorial/jmeter-properties-and-variables/)  
 [Octoperf JMeter Properties](https://octoperf.com/blog/2019/01/14/flexible-test-plans/#run-time-property-changes)
+
 # Setting Up JMeter Backend Listener
+
 # Basic Visual Flow
+
 ::: mermaid
 graph LR;
 A[Azure DevOps] -- JMeter Folder --> B(Azure Build Pipeline)
